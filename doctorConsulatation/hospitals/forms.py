bd21b2092a9104285854446faabc9b3df4eff5a3
AF Rakib <81510990+mehedi10376@users.noreply.github.com>
2021-05-26 01:19:46 +0600

    new updates

---
+++ doctorConsulatation/hospitals/forms.py
@@ -0,0 +1,25 @@
+from django.forms import ModelForm
+from django.contrib.auth.models import User
+from django import forms
+from .models import Doctor,TakeAppointment,MeetLink
+
+class DoctorEditForm(forms.ModelForm):
+    class Meta:
+        model = Doctor
+        exclude = {'user'}
+
+
+class SetDoctorProfile(forms.ModelForm):
+    class Meta:
+        model = Doctor
+        exclude = {'user'}
+
+class SetAppointment (forms.ModelForm):
+    class Meta:
+        model = TakeAppointment
+        exclude = {'user','date','is_visited','doctor'}
+
+class SetMeetlink (forms.ModelForm):
+    class Meta:
+        model = MeetLink
+        exclude = {'doctor'}
