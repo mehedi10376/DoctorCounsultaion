b40d9af6f2f04a2b3507b78eb3ee3324c152c8e9
AF Rakib <81510990+mehedi10376@users.noreply.github.com>
2021-06-07 22:38:08 +0600

    template update

--- doctorConsulatation/hospitals/admin.py
+++ doctorConsulatation/hospitals/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-
+from django.contrib.auth.models import Group
 # Register your models here.
 from django.contrib import admin
 
 from .models import *
+
+admin.site.site_header = 'Medico Consultation Admin Panel'
 
 admin.site.register(Doctor)
 
